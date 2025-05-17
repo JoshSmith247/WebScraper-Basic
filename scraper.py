91483fc52a033a55eb1b2b91102f30c3d47c16bc
JoshSmith247 <54862229+JoshSmith247@users.noreply.github.com>
2025-05-17 11:02:02 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
 
 stocks_to_find = []
 success = 0
@@ -9,13 +10,15 @@ total = 0
 print("Running Company Market Cap Scraper")
 
 options = Options()
-options.add_argument('--headless')
+#options.add_argument('--headless')
 
 driver = webdriver.Chrome(options=options)
 
 print("Collecting company list")
 
 driver.get("https://companiesmarketcap.com/")
+
+time.sleep(10)
 
 html = driver.page_source
 
@@ -25,7 +28,9 @@ for tag in soup.find_all(class_='company-name'):
     stocks_to_find.append(tag.text)
 
 #print(stocks_to_find)
-print("Collecting stock data")
+total_entries = len(stocks_to_find)
+
+print("Collecting stock data: " + str(total_entries) + " entries total.")
 
 for stock in stocks_to_find:
     
@@ -37,7 +42,7 @@ for stock in stocks_to_find:
         soup = BeautifulSoup(html, features="html.parser")
 
         for tag in soup.find(class_='stocks-module__currentPrice'):
-            print(stock.capitalize() + ": " + tag.text)
+            print("(" + str(total + 1) + "/" + str(total_entries) + ") " + stock.capitalize() + ": " + tag.text)
             success += 1
 
     except:
