25eba61319aa7a6cd36e949bdd36cecbe6ac1aa6
Josh S <54862229+JoshSmith247@users.noreply.github.com>
2025-05-17 12:40:10 -0500

    Switch to bing, comments

--- scraper.py
+++ scraper.py
@@ -1,6 +1,14 @@
+#
+# Basic web scraper–– Retrieves list of 100 companies with the highest market
+# cap, scrapes prices individually.
+# 
+# Requires bs4 and selenium
+# 
+# @author Josh Smith
+#
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import time
 
 stocks_to_find = []
@@ -9,39 +17,42 @@ total = 0
 
 print("Running Company Market Cap Scraper")
 
-options = Options()
-#options.add_argument('--headless')
+# Initialize browser tool
 
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
+
+# Scrape top 100 companies from companiesmarketcap.com
 
 print("Collecting company list")
 
 driver.get("https://companiesmarketcap.com/")
 
-time.sleep(10)
+time.sleep(3)
 
 html = driver.page_source
 
 soup = BeautifulSoup(html, features="html.parser")
 
 for tag in soup.find_all(class_='company-name'):
-    stocks_to_find.append(tag.text)
+    stocks_to_find.append(tag.text.replace("&", "and"))
 
 #print(stocks_to_find)
 total_entries = len(stocks_to_find)
+
+# Parse stock list–– search price by class from bing homepage
 
 print("Collecting stock data: " + str(total_entries) + " entries total.")
 
 for stock in stocks_to_find:
     
     try:
-        driver.get('https://duckduckgo.com/?t=h_&q=' + stock + " stock")
+        driver.get('https://www.bing.com/search?q=' + stock + ' stock')
 
         html = driver.page_source
 
         soup = BeautifulSoup(html, features="html.parser")
 
-        for tag in soup.find(class_='stocks-module__currentPrice'):
+        for tag in soup.find(class_='b_focusTextMedium'):
             print("(" + str(total + 1) + "/" + str(total_entries) + ") " + stock.capitalize() + ": " + tag.text)
             success += 1
 
@@ -50,4 +61,5 @@ for stock in stocks_to_find:
     
     total += 1
 
+# Print success rate (for debugging)
 print("Query success rate: " + str(success) + "/" + str(total) + " (" + str(success / total * 100)  + " %)")
